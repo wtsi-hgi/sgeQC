ee8379ee0abbff48f70fe043649dc21f18ecf6f1
fs18 <fs18@sanger.ac.uk>
2023-07-13 12:11:22 +0100

    minor fix on qc_reports

--- R/qc_reports.R
+++ R/qc_reports.R
@@ -23,8 +23,8 @@ create_qc_reports <- function(samplesheet = NULL,
         # creating reports #
         #------------------#
         report_path <- paste0(qcdir, "/", "MAVEQC_report.Rmd")
-
         sink(report_path)
+
         cat("---", "\n", sep = "")
         cat("title: \"MAVE QC Report\"", "\n", sep = "")
         cat("date: \"`r Sys.Date()`\"", "\n", sep = "")
@@ -33,42 +33,37 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("\n", sep = "")
 
         cat("```{r setup, include = FALSE}", "\n", sep = "")
-        cat("knitr::opts_chunk$set(echo = TRUE)", "\n", sep = "")
+        cat("knitr::opts_chunk$set(echo = TRUE, fig.align = \"center\")", "\n", sep = "")
+        cat("library(reactable)", "\n", sep = "")
+        cat("outdir <- \"", qcdir, "\"", "\n", sep = "")
         cat("```", "\n", sep = "")
         cat("\n", sep = "")
 
         cat("---", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("## Introduction", "\n", sep = "")
-        cat("MAVEQC is a flexible R-package that provides QC analysis of Saturation Genome Editing (SGE) experimental data. 
-             Available under GPL 3.0 from https://github.com/wtsi-hgi/MAVEQC", "\n", sep = "")
-        cat("\n", sep = "")
-
-        cat("---", "\n", sep = "")
-        cat("\n", sep = "")
-
-        cat("## Analysis", "\n", sep = "")
-        cat("Displays QC plots and statistics for all samples for QC.", "\n", sep = "")
+        cat("## 1. Introduction", "\n", sep = "")
+        cat("MAVEQC is a flexible R-package that provides QC analysis of Saturation Genome Editing (SGE) experimental data. ",
+            "Available under GPL 3.0 from https://github.com/wtsi-hgi/MAVEQC", "\n", sep = "")
         cat("\n", sep = "")
 
         cat("---", "\n", sep = "")
         cat("\n", sep = "")
 
         if (qctype == "screen") {
-            cat("### Screen QC", "\n", sep = "")
+            cat("### 2. Screen QC", "\n", sep = "")
         } else {
-            cat("### Plasmid QC", "\n", sep = "")
+            cat("### 2. Plasmid QC", "\n", sep = "")
         }
+        cat("Displays QC plots and statistics for all samples for QC.", "\n", sep = "")
         cat("\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
-        cat("library(reactable)", "\n", sep = "")
         cat("samqc_cutoffs <- as.data.frame(read.table(\"", qcdir, "/sample_qc_cutoffs.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
         cat("```", "\n", sep = "")
         cat("<br>", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("#### Sample Sheet", "\n", sep = "")
+        cat("#### 2.1. Sample Sheet", "\n", sep = "")
         cat("\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
         cat("df <- as.data.frame(read.table(\"", samplesheet, "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
@@ -78,12 +73,15 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("<br>", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("#### Run Sample QC", "\n", sep = "")
+        cat("#### 2.2. Run Sample QC", "\n", sep = "")
         cat("\n", sep = "")
-        cat("* ##### Read Length Distrubtion", "\n", sep = "")
+        cat("##### 2.2.1. Read Length Distrubtion", "\n", sep = "")
         cat("Displays the percentage of reads for each sample, based on 50 nucleotide increments.", "\n", sep = "")
         cat("\n", sep = "")
-        cat("![](", qcdir, "/sample_qc_read_length.png){height=50% width=50%}", "\n", sep = "")
+
+        cat("```{r, echo = FALSE, out.height = \"50%\", out.width = \"50%\"}", "\n", sep = "")
+        cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_read_length.png\"), rel_path = FALSE)", "\n", sep = "")
+        cat("```", "\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
         cat("df <- as.data.frame(read.table(\"", qcdir, "/sample_qc_read_length.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
         cat("reactable(df, highlight = TRUE, bordered = TRUE,  striped = TRUE, compact = TRUE, wrap = TRUE,", "\n", sep = "")
@@ -96,15 +94,17 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("<br>", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("* ##### Total Reads", "\n", sep = "")
-        cat("Displays the total number of reads per sample. 
-             Filtering based on 1-dimensional Kmean clustering that excludes unique sequences with read counts below", 
-             "samqc_cutoffs$total_reads", "\n", sep = "")
-        cat("Accepted reads - Total read count for all unique sequences with sufficient reads.", "\n", sep = "")
-        cat("Excluded reads - Total read count for all unique sequences with insufficient reads.", "\n", sep = "")
+        cat("##### 2.2.2. Total Reads", "\n", sep = "")
+        cat("Displays the total number of reads per sample. ",
+            "Filtering based on 1-dimensional Kmean clustering that excludes unique sequences with low read counts.", "\n", sep = "")
+        cat("\n", sep = "")
+        cat("* **Accepted reads**: Total read count for all unique sequences with sufficient reads.", "\n", sep = "")
+        cat("* **Excluded reads**: Total read count for all unique sequences with insufficient reads.", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("![](", qcdir, "/sample_qc_stats_total.png){height=50% width=50%}", "\n", sep = "")
+        cat("```{r, echo = FALSE, out.height = \"50%\", out.width = \"50%\"}", "\n", sep = "")
+        cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_stats_total.png\"), rel_path = FALSE)", "\n", sep = "")
+        cat("```", "\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
         cat("df <- as.data.frame(read.table(\"", qcdir, "/sample_qc_stats_total.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
         cat("reactable(df, highlight = TRUE, bordered = TRUE,  striped = TRUE, compact = TRUE, wrap = TRUE,", "\n", sep = "")
@@ -128,15 +128,18 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("<br>", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("* ##### Accepted Reads", "\n", sep = "")
+        cat("##### 2.2.3. Accepted Reads", "\n", sep = "")
         cat("Displays the percentage of library reads vs non-library reads (ie. Reference, PAM and Unmapped) for Accepted Reads.", "\n", sep = "")
-        cat("Library Reads - Percentage reads mapping to template oligo sequences, including intended variants.", "\n", sep = "")
-        cat("Reference Reads  - Percentage reads mapping to Reference.", "\n", sep = "")
-        cat("PAM_Reads - Percentage reads mapping to PAM/Protospacer Protection Edits (PPEs), without intended variant.", "\n", sep = "")
-        cat("Unmapped Reads - Percentage of Unmapped Reads.", "\n", sep = "")
+        cat("\n", sep = "")
+        cat("* **Library Reads**: Percentage reads mapping to template oligo sequences, including intended variants.", "\n", sep = "")
+        cat("* **Reference Reads**: Percentage reads mapping to Reference.", "\n", sep = "")
+        cat("* **PAM_Reads**: Percentage reads mapping to PAM/Protospacer Protection Edits (PPEs), without intended variant.", "\n", sep = "")
+        cat("* **Unmapped Reads**: Percentage of Unmapped Reads.", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("![](", qcdir, "/sample_qc_stats_accepted.png){height=50% width=50%}", "\n", sep = "")
+        cat("```{r, echo = FALSE, out.height = \"50%\", out.width = \"50%\"}", "\n", sep = "")
+        cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_stats_accepted.png\"), rel_path = FALSE)", "\n", sep = "")
+        cat("```", "\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
         cat("df <- as.data.frame(read.table(\"", qcdir, "/sample_qc_stats_accepted.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
         cat("reactable(df, highlight = TRUE, bordered = TRUE,  striped = TRUE, compact = TRUE, wrap = TRUE,", "\n", sep = "")
@@ -178,11 +181,13 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("<br>", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("* ##### Genomic Coverage", "\n", sep = "")
+        cat("##### 2.2.4. Genomic Coverage", "\n", sep = "")
         cat("Distribution of variants across targeton region based on log2(count+1) values.", "\n", sep = "")
         cat("\n", sep = "")
 
-        cat("![](", qcdir, "/sample_qc_position_cov.dots.png)", "\n", sep = "")
+        cat("```{r, echo = FALSE}", "\n", sep = "")
+        cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_position_cov.dots.png\"), rel_path = FALSE)", "\n", sep = "")
+        cat("```", "\n", sep = "")
         cat("```{r, echo = FALSE}", "\n", sep = "")
         cat("df <- as.data.frame(read.table(\"", qcdir, "/sample_qc_stats_pos_coverage.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
         cat("reactable(df, highlight = TRUE, bordered = TRUE,  striped = TRUE, compact = TRUE, wrap = TRUE,", "\n", sep = "")
@@ -207,13 +212,15 @@ create_qc_reports <- function(samplesheet = NULL,
         cat("\n", sep = "")
 
         if (qctype == "screen") {
-            cat("* ##### Genomic Position Percentage", "\n", sep = "")
-            cat("Displays distribution of \"LOF\" (loss-of-function) vs all \"Other\" variants across the targeton region, 
-                 based on read percentages for reference timepoint. 
-                 Requires concordant distribution of LOF and Other variants.", "\n", sep = "")
+            cat("##### 2.2.5. Genomic Position Percentage", "\n", sep = "")
+            cat("Displays distribution of \"LOF\" (loss-of-function) vs all \"Other\" variants across the targeton region, ",
+                "based on read percentages for reference timepoint. ",
+                "Requires concordant distribution of LOF and Other variants.", "\n", sep = "")
             cat("\n", sep = "")
 
-            cat("![](", qcdir, "/sample_qc_position_anno.lof_dots.png){height=65% width=65%}", "\n", sep = "")
+            cat("```{r, echo = FALSE, out.height = \"65%\", out.width = \"65%\"}", "\n", sep = "")
+            cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_position_anno.lof_dots.png\"), rel_path = FALSE)", "\n", sep = "")
+            cat("```", "\n", sep = "")
             cat("```{r, echo = FALSE}", "\n", sep = "")
             cat("df <- as.data.frame(read.table(\"", qcdir, "/sample_qc_stats_pos_percentage.tsv", "\", header = TRUE, sep = \"\\t\", check.names = FALSE))", "\n", sep = "")
             cat("reactable(df, highlight = TRUE, bordered = TRUE,  striped = TRUE, compact = TRUE, wrap = TRUE,", "\n", sep = "")
@@ -239,17 +246,30 @@ create_qc_reports <- function(samplesheet = NULL,
             cat("<br>", "\n", sep = "")
             cat("\n", sep = "")
 
-            cat("#### Run Experiment QC", "\n", sep = "")
+            cat("#### 2.3. Run Experiment QC", "\n", sep = "")
             cat("\n", sep = "")
-            cat("* ##### Sample Correlations", "\n", sep = "")
+            cat("##### 2.3.1. Sample Correlations", "\n", sep = "")
             cat("\n", sep = "")
-            cat("![](", qcdir, "/sample_qc_samples_corr.png){height=50% width=50%}", "\n", sep = "")
+            cat("```{r, echo = FALSE, out.height = \"50%\", out.width = \"50%\"}", "\n", sep = "")
+            cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_samples_corr.png\"), rel_path = FALSE)", "\n", sep = "")
+            cat("```", "\n", sep = "")
             cat("<br>", "\n", sep = "")
             cat("\n", sep = "")
 
-            cat("* ##### Sample PCA", "\n", sep = "")
+            cat("##### 2.3.2. Sample PCA", "\n", sep = "")
             cat("\n", sep = "")
-            cat("![](", qcdir, "/sample_qc_pca_samples.png){height=75% width=75%}", "\n", sep = "")
+            cat("```{r, echo = FALSE, out.height = \"75%\", out.width = \"75%\"}", "\n", sep = "")
+            cat("knitr::include_graphics(paste0(outdir, \"/sample_qc_pca_samples.png\"), rel_path = FALSE)", "\n", sep = "")
+            cat("```", "\n", sep = "")
+            cat("<br>", "\n", sep = "")
+            cat("\n", sep = "")
+
+            cat("##### 2.3.3. Folder Change", "\n", sep = "")
+            cat("\n", sep = "")
+            cat("```{r, echo = FALSE, out.height = \"75%\", out.width = \"75%\"}", "\n", sep = "")
+            cat("figs <- list.files(path = outdir, pattern = \"sample_qc_deseq_fc.*.violin.png\", full.names = TRUE)", "\n", sep = "")
+            cat("knitr::include_graphics(figs, rel_path = FALSE)", "\n", sep = "")
+            cat("```", "\n", sep = "")
             cat("<br>", "\n", sep = "")
             cat("\n", sep = "")
         }
